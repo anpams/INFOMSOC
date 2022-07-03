42ca2287ae7e5c1fff8afd20f212e44da934764e
anpams <anpa.eipi@gmail.com>
2022-07-03 11:16:07 +0200

    deleted text from date to body, replaced by ";"

--- main.py
+++ main.py
@@ -2,27 +2,24 @@ from striprtf.striprtf import rtf_to_text
 import os
 import re
 
-path_origin = 'C:/Users/anpae/Desktop/Data_Rtf/MirrGuard_NoDuplicates/'
-path_dest = 'C:/Users/anpae/Desktop/Data_Rtf/guardian_txt'
-name = 'mirrGuard'
+path_origin = 'C:/Users/anpae/Desktop/Data_Rtf/sun_txt/'
+path_dest = 'C:/Users/anpae/Desktop/Data_Rtf/sun1'
+name = 'sunTele'
 idx = 0
 aux = 0
-
-# regex="\b(Sunday|Monday|Tuesday|Wednesday|Friday|Saturday).*\n*\b\b.*\n*.*\n*\W*\w*\b\W*\bByline.*\b\W*\bBody"gm
-
 
 for filename in os.listdir(path_origin):
     filepath = os.path.join(path_origin, filename)
     if os.path.isfile(filepath):
         with open(filepath, 'r') as file:
-            rtf_file = file.read()  # rtf to txt
-            text = rtf_to_text(rtf_file) # writes to file
-            if (text[0:5] != "[Loca"):
+            text = file.read()  # rtf to txt
+            regex = re.sub(r'.(Sunday|Monday|Tuesday|Thursday|Wednesday|Friday|Saturday)(.*\n)*Body\n*' , ";" , text)
+            if text[0:5] != "[Loca":
+                #print(regex)
                 num = str(idx)
-                name_aux = name + "_"+num +".txt"  # name of new file
+                name_aux = name + "_" + num + ".txt"  # name of new file
                 new_filepath = os.path.join(path_dest, name_aux)
                 with open(new_filepath, 'w') as f:
-                    f.write(text.strip())
+                    f.write(regex.strip())
                     idx += 1
                     print(".")
-
