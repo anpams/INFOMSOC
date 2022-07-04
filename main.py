04b9ca85a7a99c7eb6f070e66104f9f12ce2cc7e
anpams <anpa.eipi@gmail.com>
2022-07-04 11:56:55 +0200

    fml

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-from striprtf.striprtf import rtf_to_text
 import os
 import re
 
-path_origin = 'C:/Users/anpae/Desktop/Data_Rtf/guardian2/'
-path_dest = 'C:/Users/anpae/Desktop/Data_Rtf/guardian1'
-#name = 'guardian'
+path_origin = 'C:/Users/anpae/Desktop/Data_Rtf/all/'
+path_dest = 'C:/Users/anpae/Desktop/Data_Rtf/aux_folder'
+
 idx = 0
 aux = 0
 
@@ -14,17 +13,16 @@ for filename in os.listdir(path_origin):
         with open(filepath, 'r') as file:
             text = file.read()
             if text[0:5] != "[Loca":
-                if text[0:5] == "The G":
-                    regex = re.sub(r'The Guardian\n', "The Guardian;", text)
-                    name='guardian'
-                else:
-                    regex = regex = re.sub(r'Mirror\n', "Mirror;", text)
-                    name='mirror'
-                #print(regex)
+                # regex = re.sub(r'\.;', ".", text)
+                test = re.findall(r'GMT', text)
+                if (test != ""):
+                    print(filename)
+                    # print(re.search(r';', text).group())
                 num = str(idx)
-                name_aux = name + "_" + num + ".txt"  # name of new file
-                new_filepath = os.path.join(path_dest, name_aux)
-                with open(new_filepath, 'w') as f:
-                    f.write(regex.strip())
+                # name_aux = name + "_" + num + ".txt"  # name of new file
+                new_filepath = os.path.join(path_dest, filename)
+                with open(filepath, 'w') as f:
+                    # f.write(regex.strip())
+
                     idx += 1
-                    print(".")
+                    # print(".")
